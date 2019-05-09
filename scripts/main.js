f075fd39a1e1e1bd60e3a73a18a33d28bb7be12a
crazyArtur <artur.dbsz@gmail.com>
2019-05-09 17:21:26 +1000

    Image changer functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/SC22.jpg') {
+      myImage.setAttribute ('src','images/SC2P.jpg');
+    } else {
+      myImage.setAttribute ('src','images/SC22.jpg');
+    }
+}
